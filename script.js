4191382b9bf1b7d4e4749fb10a695aa49b7cf304
ivan <ivanbazan12xd@gmail.com>
2024-10-06 13:23:17 -0300

    primeras modificaciones e interactividad

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const toggleBtn = document.querySelector('toggle-btn');
-const sidebar = document.querySelector('sideBar');
+const toggleBtn = document.querySelector('.toggle-btn');
+const sidebar = document.querySelector('.sideBar');
 const mainContent = document.querySelector('.main');
 
 toggleBtn.addEventListener('click', () => {
-
+    
+    // Alterna la clase 'collapsed' para sidebar y mainContent
     sidebar.classList.toggle('collapsed');
     mainContent.classList.toggle('collapsed');
-}
-)
-;
+
+    // Si sidebar contiene la clase 'collapsed', le añadimos 'sideBar'
+    if (sidebar.classList.contains('collapsed')) {
+        sidebar.classList.add('sideBar');
+    } else {
+        // Si no contiene la clase 'collapsed', removemos 'sideBar'
+        sidebar.classList.add('sideBar');
+    }
+
+});
+
+
